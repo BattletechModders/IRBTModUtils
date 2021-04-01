d695961102e3119c042c293ef0c54af44ec1097b
IceRaptor <gkitts@gmail.com>
2021-04-01 12:45:55 -0400

    - New interfaces for influenceMap customization in CleverGirl

---
+++ IRBTModUtils/IRBTModUtils/CustomInfluenceMap/CustomInfluenceMapAllyFactor.cs
@@ -0,0 +1,23 @@
+﻿using BattleTech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRBTModUtils.CustomInfluenceMap
+{
+	public abstract class CustomInfluenceMapAllyFactor : InfluenceMapAllyFactor
+	{
+		public override string Name => "ALWAYS OVERRIDE ME!";
+
+		// We always return INVALID_UNSET because most custom behaviors will use unique behavior variables or calcluations
+		public override BehaviorVariableName GetRegularMoveWeightBVName() { return BehaviorVariableName.INVALID_UNSET; }
+
+		public override BehaviorVariableName GetSprintMoveWeightBVName() { return BehaviorVariableName.INVALID_UNSET; }
+
+		public abstract float GetRegularMoveWeight(AbstractActor actor);
+		public abstract float GetSprintMoveWeight(AbstractActor actor);
+
+	}
+}
