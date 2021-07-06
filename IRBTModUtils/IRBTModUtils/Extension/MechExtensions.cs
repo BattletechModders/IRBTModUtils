72f20541e37ec82c54f5dd5eefdb5422294d040c
CMiSSioN <47270663+CMiSSioN@users.noreply.github.com>
2021-07-06 12:43:40 +0300

    CU compatibility API

--- IRBTModUtils/IRBTModUtils/Extension/MechExtensions.cs
+++ IRBTModUtils/IRBTModUtils/Extension/MechExtensions.cs
@@ -1,11 +1,22 @@
 ﻿using BattleTech;
 using System;
+using System.Collections.Generic;
 
 namespace IRBTModUtils.Extension
 {
     public static class MechExtensions
     {
-        public static float ModifiedWalkDistance(this Mech mech)
+        private static List<Func<Mech, float, float>> extWalkMods = new List<Func<Mech, float, float>>();
+        private static List<Func<Mech, float, float>> extRunMods = new List<Func<Mech, float, float>>();
+        public static void AddExternalWalkModifier(Func<Mech, float, float> mod) { extWalkMods.Add(mod); }
+        public static void AddExternalRunModifier(Func<Mech, float, float> mod) { extRunMods.Add(mod); }
+        public static float ModifiedWalkDistance(this Mech mech) {
+          return ModifiedWalkDistanceExt(mech, true);
+        }
+        public static float ModifiedRunDistance(this Mech mech) {
+          return ModifiedRunDistanceExt(mech, true);
+        }
+        public static float ModifiedWalkDistanceExt(this Mech mech, bool extModifiers = true)
         {
             Mod.Log.Info?.Write($"ModifiedWalkDistance for: {mech.DistinctId()}");
             float walkDistance = 0f;
@@ -20,11 +31,11 @@ namespace IRBTModUtils.Extension
             walkDistance = (float)Math.Ceiling(mech.WalkSpeed - total);
             if (walkDistance < Mod.Config.MinimumMove)
                 walkDistance = Mod.Config.MinimumMove;
-
+            if(extModifiers) foreach (Func<Mech, float, float> extmod in extWalkMods) { walkDistance = extmod(mech, walkDistance); }
             return walkDistance;
         }
 
-        public static float ModifiedRunDistance(this Mech mech)
+        public static float ModifiedRunDistanceExt(this Mech mech, bool extModifiers = true)
         {
             Mod.Log.Info?.Write($"ModifiedRunDistance for: {mech.DistinctId()}");
             float runDistance = 0f;
@@ -39,7 +50,7 @@ namespace IRBTModUtils.Extension
             runDistance = (float)Math.Ceiling(mech.RunSpeed - total);
             if (runDistance < Mod.Config.MinimumMove)
                 runDistance = Mod.Config.MinimumMove;
-
+            if(extModifiers) foreach (Func<Mech, float, float> extmod in extRunMods) { runDistance = extmod(mech, runDistance); }
             return runDistance;
         }
 
