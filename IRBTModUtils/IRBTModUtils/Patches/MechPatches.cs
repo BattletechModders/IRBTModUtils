f2f0b7d8719efa77a85e7489926b9b05e051a7b9
IceRaptor <gkitts@gmail.com>
2021-07-18 11:16:12 -0400

    no message

--- IRBTModUtils/IRBTModUtils/Patches/MechPatches.cs
+++ IRBTModUtils/IRBTModUtils/Patches/MechPatches.cs
@@ -1,11 +1,6 @@
 ﻿using BattleTech;
 using Harmony;
 using IRBTModUtils.Extension;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IRBTModUtils.Patches
 {
