6d5e1969c89a27e881c3fe2faba9ce3c138f01fa
CMiSSioN <47270663+CMiSSioN@users.noreply.github.com>
2021-12-06 17:18:54 +0300

    gif storage update

--- IRBTModUtils/IRBTModUtils/Helper/GifStorage.cs
+++ IRBTModUtils/IRBTModUtils/Helper/GifStorage.cs
@@ -27,6 +27,33 @@ namespace IRBTModUtils {
       }
       return null;
     }
+    public static UniGif.GifSprites GetSprites(string id){
+      if (string.IsNullOrEmpty(id) == false) {
+        if (gifSprites.TryGetValue(id, out var result)) {
+          return result;
+        }
+      }
+      return null;
+    }
+    public static UniGif.GifSprites GetPortraitGifSprite(this PilotDef pilotDef) {
+      if (pilotDef.PortraitSettings != null) {
+        if (pilotDef.PortraitSettings.Description != null) {
+          if (string.IsNullOrEmpty(pilotDef.PortraitSettings.Description.Icon) == false) {
+            if (gifSprites.TryGetValue(pilotDef.PortraitSettings.Description.Icon, out var result)) {
+              return result;
+            }
+          }
+        }
+      }
+      if (pilotDef.Description != null) {
+        if (string.IsNullOrEmpty(pilotDef.Description.Icon) == false) {
+          if (gifSprites.TryGetValue(pilotDef.Description.Icon, out var result)) {
+            return result;
+          }
+        }
+      }
+      return null;
+    }
     public static UniGif.GifImage GetPortraitGifImage(this Pilot pilot) {
       if (pilot == null) { return null; }
       if (pilot.pilotDef.PortraitSettings != null) {
@@ -47,9 +74,36 @@ namespace IRBTModUtils {
       }
       return null;
     }
+    public static UniGif.GifImage GetPortraitGifImage(this PilotDef pilotDef) {
+      if (pilotDef.PortraitSettings != null) {
+        if (pilotDef.PortraitSettings.Description != null) {
+          if (string.IsNullOrEmpty(pilotDef.PortraitSettings.Description.Icon) == false) {
+            if (gifImages.TryGetValue(pilotDef.PortraitSettings.Description.Icon, out var result)) {
+              return result;
+            }
+          }
+        }
+      }
+      if (pilotDef.Description != null) {
+        if (string.IsNullOrEmpty(pilotDef.Description.Icon) == false) {
+          if (gifImages.TryGetValue(pilotDef.Description.Icon, out var result)) {
+            return result;
+          }
+        }
+      }
+      return null;
+    }
     public static UniGif.GifImage GetDescrGifImage(this BaseDescriptionDef descr) {
       if (string.IsNullOrEmpty(descr.Icon) == false) {
         if (gifImages.TryGetValue(descr.Icon, out var result)) {
+          return result;
+        }
+      }
+      return null;
+    }
+    public static UniGif.GifSprites GetDescrGifSprite(this BaseDescriptionDef descr) {
+      if (string.IsNullOrEmpty(descr.Icon) == false) {
+        if (gifSprites.TryGetValue(descr.Icon, out var result)) {
           return result;
         }
       }
