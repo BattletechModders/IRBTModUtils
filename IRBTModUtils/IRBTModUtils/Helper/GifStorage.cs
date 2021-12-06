a733d80f35d28b7487d0db439f0d4df1d562866e
CMiSSioN <47270663+CMiSSioN@users.noreply.github.com>
2021-12-06 12:54:51 +0300

    gif storage update

--- IRBTModUtils/IRBTModUtils/Helper/GifStorage.cs
+++ IRBTModUtils/IRBTModUtils/Helper/GifStorage.cs
@@ -8,14 +8,30 @@ namespace IRBTModUtils {
     private static Dictionary<string, UniGif.GifSprites> gifSprites = new Dictionary<string, UniGif.GifSprites>();
     //private static Dictionary<int, string> imageDataId = new Dictionary<int, string>();
     public static UniGif.GifSprites GetPortraitGifSprite(this Pilot pilot) {
-      if (gifSprites.TryGetValue(pilot.pilotDef.Description.Icon, out var result)) {
-        return result;
+      if (pilot == null) { return null; }
+      if (pilot.pilotDef.PortraitSettings != null) {
+        if (pilot.pilotDef.PortraitSettings.Description != null) {
+          if (string.IsNullOrEmpty(pilot.pilotDef.PortraitSettings.Description.Icon) == false) {
+            if (gifSprites.TryGetValue(pilot.pilotDef.PortraitSettings.Description.Icon, out var result)) {
+              return result;
+            }
+          }
+        }
+      }
+      if (pilot.pilotDef.Description != null) {
+        if (string.IsNullOrEmpty(pilot.pilotDef.Description.Icon) == false) {
+          if (gifSprites.TryGetValue(pilot.pilotDef.Description.Icon, out var result)) {
+            return result;
+          }
+        }
       }
       return null;
     }
-    public static UniGif.GifImage GetPortraitGifImage(this BaseDescriptionDef descr) {
-      if (gifImages.TryGetValue(descr.Icon, out var result)) {
-        return result;
+    public static UniGif.GifImage GetDescrGifImage(this BaseDescriptionDef descr) {
+      if (string.IsNullOrEmpty(descr.Icon) == false) {
+        if (gifImages.TryGetValue(descr.Icon, out var result)) {
+          return result;
+        }
       }
       return null;
     }
@@ -23,10 +39,18 @@ namespace IRBTModUtils {
       return (data[0] == 0x47) && (data[1] == 0x49) && (data[2] == 0x46);
     }
     public static void Register(this UniGif.GifImage gif, string id) {
-      gifImages.Add(id, gif);
+      if (gifImages.ContainsKey(id) == false) {
+        gifImages.Add(id, gif);
+      } else {
+        gifImages[id] = gif;
+      }
     }
     public static void Register(this UniGif.GifSprites gif, string id) {
-      gifSprites.Add(id, gif);
+      if (gifSprites.ContainsKey(id) == false) {
+        gifSprites.Add(id, gif);
+      } else {
+        gifSprites[id] = gif;
+      }
     }
     //public static void Register(this Sprite sprite, string id) {
     //  imageDataId.Add(sprite.GetInstanceID(), id);
