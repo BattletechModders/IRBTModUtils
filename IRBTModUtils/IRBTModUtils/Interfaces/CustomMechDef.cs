45fa691b27f1c3fdf3e057fb8aff4444c2d238f2
KMiSSioN <missionimp@mail.ru>
2024-03-28 11:42:05 +0300

    improvements

---
+++ IRBTModUtils/IRBTModUtils/Interfaces/CustomMechDef.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRBTModUtils.Interfaces
+{
+    public interface ICustomMechDef
+    {
+        bool isSquad { get; }
+        bool isVehicle { get; }
+        bool isQuad { get; }
+        bool isTurret { get; }
+        string UnitTypeName { get; }
+        Dictionary<ChassisLocations, string> locationNames { get; }
+    }
+}
